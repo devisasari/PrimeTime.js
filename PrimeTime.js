17dbab1238f762ba32c8f8212fcd9b8d9379c6ba
devisasari <dev.isasari@yandex.com>
2022-04-19 20:16:29 +0300

    first commit

---
+++ PrimeTime.js
@@ -0,0 +1,16 @@
+/*
+Prime Time
+
+Have the function PrimeTime(num) take the num parameter being passed and return the string true if the parameter is a prime number, otherwise return the string false. The range will be between 1 and 2^16. 
+*/
+
+function PrimeTime( num ) {
+    for ( var i = 2; i < num; i++ ) {
+      if ( num % i == 0 )
+        return false;
+    }
+    
+    return true;
+  }
+  console.log(PrimeTime(readline()));
+  
